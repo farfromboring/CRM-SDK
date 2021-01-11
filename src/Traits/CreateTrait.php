c99e3e767d32026ada25eca7431e2313812f515c
Shane Stebner <shane@htmlguy.com>
2021-01-10 21:10:12 -0500

    Add default params to create method

--- src/Traits/CreateTrait.php
+++ src/Traits/CreateTrait.php
@@ -4,11 +4,14 @@ namespace CRM_SDK\Traits;
 trait CreateTrait
 {
     /**
+     * @param string|null $api_key
+     * @param int|null $version
+     * @param \GuzzleHttp\Client|null $client
      * @return static
      */
-    public static function create()
+    public static function create(?string $api_key = null, ?int $version = null, ?\GuzzleHttp\Client $client = null)
     {
         $called_class = static::class;
-        return new $called_class();
+        return new $called_class($api_key, $version, $client);
     }
 }
